26d6c03748fb8f40195fd273aacbbe8ec1839301
Panneerselvam, Rajkumar <prajkumarp@gmail.com>
2015-03-18 14:36:30 -0400

    Base project

---
+++ Hub/Hub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hub
+//
+//  Created by Panneerselvam, Rajkumar on 3/18/15.
+//  Copyright (c) 2015 Panneerselvam, Rajkumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hub/Hub/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Hub
+//
+//  Created by Panneerselvam, Rajkumar on 3/18/15.
+//  Copyright (c) 2015 Panneerselvam, Rajkumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
